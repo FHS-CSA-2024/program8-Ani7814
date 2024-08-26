2d53f7994ed2723c8c7a591d4e0d68bfa76b4994
Ani7814 <anir7814@gmail.com>
2024-08-26 15:30:35 -0700

    Update Program.java

--- Program.java
+++ Program.java
@@ -1,6 +1,37 @@
 //import stuff here
-
+import java.util.Scanner;
 //Your code here
+
+public class Program8{
+    public static void main(String[] args){
+        //input
+        Scanner calc = new Scanner(System.in);
+        int num1;
+        int num2;
+        System.out.println("Enter number 1: ");
+        num1 = calc.nextInt();
+        System.out.println("Enter number 2: ");
+        num2 = calc.nextInt();
+        
+       
+        
+        
+          
+    
+    
+    
+    }
+    
+    
+     //calculate
+    public int sum(int number1, int number2){
+        return(number1+number2);
+    }
+    
+    public int difference(int number1, int number2){
+        return(number1-number2);
+    }
+}
 
 //Paste console output below:
 /*
