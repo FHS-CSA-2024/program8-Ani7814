9099a0c73e203d3506a5eed498e14d355f30537e
Ani7814 <anir7814@gmail.com>
2024-08-28 12:09:30 -0700

    Update Program.java

--- Program.java
+++ Program.java
@@ -13,25 +13,44 @@ public class Program8{
         System.out.println("Enter number 2: ");
         num2 = calc.nextInt();
         
-       
-        
-        
-          
-    
-    
-    
     }
     
     
      //calculate
     public int sum(int number1, int number2){
-        return(number1+number2);
+        return number1+number2;
     }
     
     public int difference(int number1, int number2){
         return(number1-number2);
     }
-}
+    public int product(int number1, int number2){
+        return(number1*number2);
+    }
+    public double average(int number1, int number2){
+        return (double)number1+(double)number2/2.0;
+    }
+    public int max(int number1, int number2){
+        if (number1>number2){
+            return number1; 
+        }
+        
+        else if(number1<number2){
+            return number2;
+        }
+    }
+    public int min(int number1, int number2){
+        if(number1>number2){
+            return number2;
+        }
+        else if(number1<number2){
+            return number1;
+        }
+    }
+    
+    }
+    
+    
 
 //Paste console output below:
 /*
