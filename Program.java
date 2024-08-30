2083130925226b6aa6cfbfc01b692c2a1121370d
Ani7814 <anir7814@gmail.com>
2024-08-30 14:23:01 -0700

    Update Program.java
    completed a part of prog 8, need changes

--- Program.java
+++ Program.java
@@ -3,34 +3,41 @@ import java.util.Scanner;
 //Your code here
 
 public class Program8{
+    
     public static void main(String[] args){
-        //input
         Scanner calc = new Scanner(System.in);
         int num1;
         int num2;
-        System.out.println("Enter number 1: ");
+        System.out.println("Enter number1: ");
         num1 = calc.nextInt();
-        System.out.println("Enter number 2: ");
+        System.out.println("Enter number2: ");
         num2 = calc.nextInt();
-        
+        System.out.println("Sum = "+sum(num1, num2));
+        System.out.println("difference = "+difference(num1,num2));
+        System.out.println("product = "+product(num1, num2));
+        System.out.println("average = "+average(num1, num2));
+        System.out.println("maximum = "+max(num1, num2));
+        System.out.println("minimum = "+min(num1, num2));
+    
     }
     
-    
      //calculate
-    public int sum(int number1, int number2){
+    public static int sum(int number1, int number2){
+
         return number1+number2;
     }
     
-    public int difference(int number1, int number2){
+    public static int difference(int number1, int number2){
         return(number1-number2);
     }
-    public int product(int number1, int number2){
+    
+    public static int product(int number1, int number2){
         return(number1*number2);
     }
-    public double average(int number1, int number2){
+    public static double average(int number1, int number2){
         return (double)number1+(double)number2/2.0;
     }
-    public int max(int number1, int number2){
+    public static int max(int number1, int number2){
         if (number1>number2){
             return number1; 
         }
@@ -38,13 +45,20 @@ public class Program8{
         else if(number1<number2){
             return number2;
         }
+        else{
+            return (0);
+        }
     }
-    public int min(int number1, int number2){
+    
+    public static int min(int number1, int number2){
         if(number1>number2){
             return number2;
         }
         else if(number1<number2){
             return number1;
+        }
+        else{
+            return (0);
         }
     }
     
@@ -54,6 +68,15 @@ public class Program8{
 
 //Paste console output below:
 /*
-
+Enter number1: 
+13
+Enter number2: 
+20
+sum = 33
+difference = -7
+product = 260
+average = 23.0
+maximum = 20
+minimum = 13
 
 */
